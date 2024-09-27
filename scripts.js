013d7eaba816273ec78a25857860c1d3746a9bf5
kahme <157324720+kahmelegoat@users.noreply.github.com>
2024-09-27 22:10:34 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.continent').forEach(continent => {
-    continent.addEventListener('click', () => {
-        const continentName = continent.getAttribute('data-continent');
-        window.location.href = `${continentName}.html`;
-    });
-});
